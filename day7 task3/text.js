a7976dd513bc4205594da065c8f119c4f8a2882e
sankethmesta <55999209+sankethmesta@users.noreply.github.com>
2021-10-24 07:25:07 -0700

    Add files via upload

---
+++ day7 task3/text.js
@@ -0,0 +1,34 @@
+
+function addition (a,b){
+       var sum=a+b;
+       return sum;
+}
+
+
+function multi(a,b){
+    var multi=a*b;
+    (console.log("Multiplication of the number is: "+multi));
+}
+
+function subtraction(a,b){
+    var sub=a-b;
+    (console.log("subtraction of the number is: "+sub));
+}
+
+function div(a,b){
+    var div=a/b;
+    (console.log("division of the number is: "+div));
+}
+
+function mod(a,b){
+    var mod=a%b;
+    (console.log("modules of the number is: "+mod));
+}
+
+addition(4,5);
+multi(2,3);
+subtraction(4,6);
+div(5,7);
+mod(4,5);
+
+
